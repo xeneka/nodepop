994ea9009a000f2c4680ac30acb9c1765b7fd672
Antonio Benavente del Moral <antonio@benavente-cardador.com>
2016-05-05 12:49:51 +0200

    Versión con Cluster. Se ha añadido la funcionalidad para que corra en cluster. En caso de que se caiga un worker levanta otro

--- app.js
+++ app.js
@@ -1,72 +1,77 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+/*
+Antonio Benavente
+ */
+'use strict';
+  var express = require('express');
+  var path = require('path');
+  var favicon = require('serve-favicon');
+  var logger = require('morgan');
+  var cookieParser = require('cookie-parser');
+  var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+  var routes = require('./routes/index');
+  var users = require('./routes/users');
 
-var app = express();
+  var app = express();
+
 
 //  Base de datos
-require('./lib/connectMongoose');
-require('./models/Usuario');
-require('./models/Anuncio');
-require('./models/Token');
+  require('./lib/connectMongoose');
+  require('./models/Usuario');
+  require('./models/Anuncio');
+  require('./models/Token');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use( express.static( path.join(__dirname, 'public') ) );
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+  app.use('/', routes);
+  app.use('/users', users);
 
 
-app.use('/api/v1/usuarios',require('./routes/api/v1/usuarios'));
-app.use('/api/v1/anuncios',require('./routes/api/v1/anuncios'));
-app.use('/api/v1/token',require('./routes/api/v1/token'));
+  app.use('/api/v1/usuarios', require('./routes/api/v1/usuarios'));
+  app.use('/api/v1/anuncios', require('./routes/api/v1/anuncios'));
+  app.use('/api/v1/token', require('./routes/api/v1/token'));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+  if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
     });
-  });
-}
+  }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
   });
-});
 
 
-module.exports = app;
+  module.exports = app;
